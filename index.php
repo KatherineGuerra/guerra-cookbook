5f66ec07db6920ac4144994261013b47043b6df8
katherine guerra <guerrakn@.gmail>
2015-05-14 11:27:56 -0700

    5-14

--- index.php
+++ index.php
@@ -7,9 +7,17 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.css">
+        <link type="text/css" rel="stylesheet" href="css/CSS.css">
         <title></title>
     </head>
     <body>
+        <div class="jumbotron">
+            <h1>Dessert book</h1>
+            <p>This is where you can find dessert recipes</p>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Join Now</a></p>
+        </div>
         <?php
         // put your code here
         ?>
